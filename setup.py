de37c7ec1dd58ef96653404a3bbe03f79ad6fcc6
Stanislav Zhelnio <zhelniosl@yandex.ru>
2020-08-13 20:12:25 +0300

    fix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     py_modules = ['drawio-layer'],
     entry_points={
         "console_scripts": [
-            "drawio-layer = drawio-layer:main",
+            r"drawio-layer = drawio-layer:main",
         ]
     },
     install_requires=requirements,
